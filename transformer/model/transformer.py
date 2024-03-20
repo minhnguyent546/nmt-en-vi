fix masking problem: mask can implicitly be set to None and cause some bugs

transformer/model/transformer.py
@@ -64,31 +64,53 @@ class Transformer(nn.Module):
         self,
         src: Tensor,
         target: Tensor,
-        src_mask: Tensor | None = None,
+        src_mask: Tensor,
         target_mask: Tensor | None = None,
-        src_is_encoded: bool = True,
     ) -> Tensor:
         """
         Args:
             src (Tensor): encoder output, shape ``(batch_size, src_seq_length, d_model)``
             target (Tensor): target tensor, shape ``(batch_size, target_seq_length)``
-            src_mask (Tensor): mask tensor for ``src``, shape ``(batch_size, 1, 1, src_seq_length)`` (default: None)
+            src_mask (Tensor): mask tensor for ``src``, shape ``(batch_size, 1, 1, src_seq_length)``
             target_mask (Tensor): mask tensor for ``target``, shape ``(batch_size, 1, target_seq_length, target_seq_length)`` (default: None)
-            src_is_encoded (bool): whether src is passed through encoder or not (default: True)
 
         Returns:
             Tensor: the output tensor, shape ``(batch_size, seq_length, d_model)``
         """
 
         if target_mask is None:
-            fun.create_decoder_mask(target, self.target_pad_token_id, has_batch_dim=True)
+            target_mask = fun.create_decoder_mask(target, self.target_pad_token_id, has_batch_dim=True)
         target = self.target_embed(target)
         target = self.target_pe(target)
 
-        if not src_is_encoded:
-            src = self.encode(src, src_mask)
-
+        assert src.dim() == 3
         target = self.decoder(src, target, src_mask, target_mask)
+        return target
+
+    def forward(
+        self,
+        src: Tensor,
+        target: Tensor,
+        src_mask: Tensor | None = None,
+        target_mask: Tensor | None = None,
+    ) -> Tensor:
+        """
+        Args:
+            src (Tensor): source tensor, shape ``(batch_size, src_seq_length)``
+            target (Tensor): target tensor, shape ``(batch_size, target_seq_length)``
+            src_mask (Tensor): mask tensor for ``src``, shape ``(batch_size, 1, 1, src_seq_length)`` (default: None)
+            target_mask (Tensor): mask tensor for ``target``, shape ``(batch_size, 1, target_seq_length, target_seq_length)`` (default: None)
+
+        Returns:
+            Tensor: the output tensor, shape ``(batch_size, seq_length, d_model)``
+        """
+        assert src.dim() == 2
+        assert target.dim() == 2
+
+        if src_mask is None:
+            src_mask = fun.create_encoder_mask(src, self.src_pad_token_id, has_batch_dim=True)
+        src = self.encode(src, src_mask=src_mask)
+        target = self.decode(src, target, src_mask=src_mask, target_mask=target_mask)
         return target
 
     def linear(self, x: Tensor) -> Tensor:

utils/model.py
@@ -11,7 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tokenizers import Tokenizer
 
 from transformer import Transformer, make_transformer
-from transformer.utils.functional import create_causal_mask
+import transformer.utils.functional as fun
 import constants as const
 
 def make_model(
@@ -67,8 +67,6 @@ def decode_with_teacher_forcing(
     device: torch.device,
     encoder_input: Tensor,
     decoder_input: Tensor,
-    encoder_mask: Tensor,
-    decoder_mask: Tensor,
     has_batch_dim: bool = False,
 ) -> Tensor:
     """
@@ -77,24 +75,20 @@ def decode_with_teacher_forcing(
         device (torch.device): device
         encoder_input (Tensor): encoder input
         decoder_input (Tensor): decoder input
-        encoder_mask (Tensor): mask tensor for encoder input
-        decoder_mask (Tensor): mask tensor for decoder input
         has_batch_dim (bool): whether input tensors have batch dimension (default: False)
 
     Returns:
         Tensor: tensor of predicted token ids
     """
+
     encoder_input = encoder_input.to(device)
     decoder_input = decoder_input.to(device)
-    encoder_mask = encoder_mask.to(device)
-    decoder_mask = decoder_mask.to(device)
 
     if not has_batch_dim:
         encoder_input.unsqueeze_(0)
         decoder_input.unsqueeze_(0)
 
-    encoder_output = model.encode(encoder_input, encoder_mask)  # (batch_size, seq_length, d_model)
-    decoder_output = model.decode(encoder_output, decoder_input, encoder_mask, decoder_mask)  # (batch_size, seq_length, d_model)
+    decoder_output = model(encoder_input, decoder_input)  # (batch_size, seq_length, d_model)
     logits = model.linear(decoder_output)  # (batch_size, seq_length, target_vocab_size)
 
     pred_token_ids = logits.argmax(dim=-1)  # (batch_size, seq_length)
@@ -105,7 +99,6 @@ def greedy_search_decode(
     model: Transformer,
     device: torch.device,
     encoder_input: Tensor,
-    encoder_mask: Tensor,
     target_tokenizer: Tokenizer,
     seq_length: int,
 ) -> Tensor:
@@ -114,9 +107,8 @@ def greedy_search_decode(
         model (Transformer): model to be used for decoding
         device (torch.device): device
         encoder_input (Tensor): encoder input
-        encoder_mask (Tensor): mask tensor for encoder input
         target_tokenizer (Tokenizer): target tokenizer
-        seq_lenght (int): maximum sequence length
+        seq_length (int): maximum sequence length
 
     Returns:
         Tensor: tensor of predicted token ids
@@ -126,17 +118,18 @@ def greedy_search_decode(
     eos_token_id = target_tokenizer.token_to_id(const.EOS_TOKEN)
 
     encoder_input = encoder_input.unsqueeze(0).to(device)
-    encoder_mask = encoder_mask.unsqueeze(0).to(device)
-    encoder_output = model.encode(encoder_input, encoder_mask)
+    encoder_mask = fun.create_encoder_mask(encoder_input, model.src_pad_token_id, has_batch_dim=True)
+    encoder_output = model.encode(encoder_input, src_mask=encoder_mask)
 
     # initialize decoder input which contains only <SOS> token
     decoder_input = torch.empty((1, 1)).fill_(sos_token_id).type_as(encoder_input).to(device)
     for _ in range(seq_length):
         # create mask for decoder input
-        decoder_mask = create_causal_mask(decoder_input.size(1)).type_as(encoder_mask).to(device)
+        decoder_mask = fun.create_causal_mask(decoder_input.size(1)).to(device)
 
         # decode
-        decoder_output = model.decode(encoder_output, decoder_input, encoder_mask, decoder_mask)
+        decoder_output = model.decode(encoder_output, decoder_input,
+                                      src_mask=encoder_mask, target_mask=decoder_mask)
 
         # get token with highest probability
         logits = model.linear(decoder_output[:, -1, :])  # (1, target_vocab_size)
@@ -165,7 +158,6 @@ def beam_search_decode(
     device: torch.device,
     beam_size: int,
     encoder_input: Tensor,
-    encoder_mask: Tensor,
     target_tokenizer: Tokenizer,
     seq_length: int,
 ) -> list[Tensor]:
@@ -175,9 +167,8 @@ def beam_search_decode(
         device (torch.device): device
         beam_size (int): beam size
         encoder_input (Tensor): encoder input
-        encoder_mask (Tensor): mask tensor for encoder input
         target_tokenizer (Tokenizer): target tokenizer
-        seq_lenght (int): maximum sequence length
+        seq_length (int): maximum sequence length
 
     Returns:
         list[Tensor]: list of candidate tensors of predicted token ids
@@ -187,8 +178,8 @@ def beam_search_decode(
     eos_token_id = target_tokenizer.token_to_id(const.EOS_TOKEN)
 
     encoder_input = encoder_input.unsqueeze(0).to(device)
-    encoder_mask = encoder_mask.unsqueeze(0).to(device)
-    encoder_output = model.encode(encoder_input, encoder_mask)
+    encoder_mask = fun.create_encoder_mask(encoder_input, model.src_pad_token_id, has_batch_dim=True)
+    encoder_output = model.encode(encoder_input, src_mask=encoder_mask)
 
     # initialize decoder input which contains only <SOS> token
     decoder_input = torch.empty((1, 1)).fill_(sos_token_id).type_as(encoder_input).to(device)
@@ -205,10 +196,11 @@ def beam_search_decode(
                 continue
 
             # create mask for decoder input
-            cand_mask = create_causal_mask(cand.size(1)).type_as(encoder_mask).to(device)
+            cand_mask = fun.create_causal_mask(cand.size(1)).to(device)
 
             # decode
-            decoder_output = model.decode(encoder_output, cand, encoder_mask, cand_mask)
+            decoder_output = model.decode(encoder_output, cand,
+                                          src_mask=encoder_mask, target_mask=cand_mask)
 
             # get next token probabilities
             # logits: shape ``(1, target_vocab_size)``
@@ -293,7 +285,7 @@ def train(
         encoder_input = batch['encoder_input'].to(device)  # (batch_size, seq_length)
         decoder_input = batch['decoder_input'].to(device)  # (batch_size, seq_length)
 
-        decoder_output = model.decode(encoder_input, decoder_input, src_is_encoded=False)  # (batch_size, seq_length, d_model)
+        decoder_output = model(encoder_input, decoder_input)  # (batch_size, seq_length, d_model)
         logits = model.linear(decoder_output)  # (batch_size, seq_length, target_vocab_size)
         labels = batch['labels'].to(device)  # (batch_size, seq_length)
 
@@ -373,7 +365,7 @@ def evaluate(
             decoder_input = batch['decoder_input'].to(device)  # (batch_size, seq_length)
             labels = batch['labels'].to(device)  # (batch_size, seq_length)
 
-            decoder_output = model.decode(encoder_input, decoder_input, src_is_encoded=False)  # (batch_size, seq_length, d_model)
+            decoder_output = model(encoder_input, decoder_input)  # (batch_size, seq_length, d_model)
             logits = model.linear(decoder_output)  # (batch_size, seq_length, target_vocab_size)
 
             # calculating the loss